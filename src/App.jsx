c34a3c84747a033f92b04bd0ff07742067475fbd
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-06-11 17:28:51 +0200

    feat: completato il form

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,43 @@ import './App.css'
 
 function App() {
 
+  const privacy = ['Privato', 'Pubblico'];
 
-const initialFormData = {
+  const initialFormData = {
 
-  autore:'',
-  titolo:'',
-  discrizione:'',
-  pubblicazione:''
+    autore: '',
+    titolo: '',
+    descrizione: '',
+    pubblicazione: 'Privato',
+    termini: false
 
-}
-
-
-const [formData, setFormData] = useState(initialFormData)
+  }
 
 
-// funzione cghe gestisce il Submit
+  const [formData, setFormData] = useState(initialFormData)
 
-const handleChange = (e) => {
 
-  const { name, value } = e.target
+  // funzione cghe gestisce il Submit
 
-  setFormData({ ...formData, [name]: value})
-  
-}
+  const handleChange = (e) => {
 
-const sendData = (e) => {
-  e.preventDefault()
-  console.log(formData)
-}
+    const { name, value, checked, type } = e.target;
+
+    setFormData({ ...formData, [name]: type === "checkbox" ? checked : value }
+
+    )
+
+    // const { name: x, value } = e.target;
+    // setFormData({ ...formData, [x]: value }) 
+    // // avendo a che fare con una destrutturazione siamo legati al fatto che il name dovrà sempre essere name e value uguale (perchè è il nome che hanno in e.taget) con i : cambiamo i nomi delle variabili destrutturate
+    console.log(formData)
+
+  }
+
+  const sendData = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
 
 
 
@@ -42,22 +51,69 @@ const sendData = (e) => {
         <form onSubmit={sendData}>
           <div className="mb-3">
             <label htmlFor="autore" className="form-label">Autore</label>
-            <input 
-            type="text" 
-            className="form-control" 
-            // questi tre campi devono essere scritti così altrimenti non funziona 
-            name = 'autore'
-            value={formData.autore}
-            onChange={handleChange}
-
-             />
+            <input
+              type="text"
+              className="form-control"
+              placeholder='...autore'
+              // questi tre campi devono essere scritti così altrimenti non funziona 
+              name='autore'
+              value={formData.autore}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="autore" className="form-label">Titolo</label>
+            <input
+              type="text"
+              className="form-control"
+              name='titolo'
+              placeholder='.. titolo'
+              value={formData.titolo}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="autore" className="form-label">Descrizione</label>
+            <textarea
+              type="text"
+              placeholder='... inserisci la descrizione'
+              className="form-control"
+              name='descrizione'
+              value={formData.descrizione}
+              onChange={handleChange}
+            />
           </div>
 
-          <div className="mb-3 form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+          <div className='row mx-1 my-4'>
+            {privacy.map((currElement, index) => (
+              <div key={index} className="mb-3 col-3 form-check">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="pubblicazione"
+                  id={currElement}
+                  value={currElement}
+                  checked={formData.pubblicazione === currElement}
+                  onChange={handleChange}
+                />
+                <label className="form-check-label" htmlFor="exampleCheck1">{currElement}</label>
+              </div>
+            ))}
           </div>
+          <div className="mb-5 form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              name="termini"
+              value={formData.termini}
+              checked={formData.termini}
+              onChange={handleChange}
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">accetta i termini ...</label>
+          </div>
+
           <button type="submit" className="btn btn-primary">Submit</button>
+
         </form>
       </div>
     </>
